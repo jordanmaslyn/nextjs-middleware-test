5de235a24af05bfa93e0924141d4d8572fd1089c
Jordan Maslyn <jordan.maslyn@wpengine.com>
2022-01-11 12:22:53 -0500

    Add security headers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,35 @@
+const securityHeaders = [
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: "default-src 'self' js.wpenginepowered.com *.js.wpenginepowered.com",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+];
+
 module.exports = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
